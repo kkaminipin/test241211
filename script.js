f08ae6008a7a56cfd857b50a3bf016c2ec643227
kkami <kkamidog@naver.com>
2024-12-11 17:53:30 +0900

    index sub 수정

--- script.js
+++ script.js
@@ -1,3 +1,2 @@
 var tl = gsap.timeline({ delay: 0 });
-//sequenced one-after-the-other
 tl.to('.box1', { duration: 2, rotation: -360 }).to('.box2', { duration: 1, x: -100, ease: 'elastic.out' }).to('.box3', { duration: 2, rotation: 360, x: 100, ease: 'expo.out' });
